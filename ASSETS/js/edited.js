9ce29c7a2ffddb0b0ae3d1a3dfe88d4ed48eb857
Saiyed Aashiya <saiyedaashiya64@gmail.com>
2025-12-22 20:02:20 +0530

    services

--- ASSETS/js/edited.js
+++ ASSETS/js/edited.js
@@ -75,18 +75,3 @@ document.addEventListener('click', function (e) {
   console.log('Offer clicked:', card.dataset.offer);
 });
 
-/*acne page navigation*/
-
-  const tabButtons = document.querySelectorAll(".tab-btn");
-  const tabPanes = document.querySelectorAll(".tab-pane");
-
-  tabButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      tabButtons.forEach(btn => btn.classList.remove("active"));
-      tabPanes.forEach(pane => pane.classList.remove("active"));
-
-      button.classList.add("active");
-      document.getElementById(button.dataset.tab).classList.add("active");
-    });
-  })
-
